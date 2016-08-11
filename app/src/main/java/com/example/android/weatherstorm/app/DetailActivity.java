ed28c7e4f2608b8d3780e2b5a53a77cd1cf3353f
Aleksandar Ilic <alexilic92@gmail.com>
2016-08-11 21:27:29 +0200

    refactor

--- app/src/main/java/com/example/android/weatherstorm/app/DetailActivity.java
+++ app/src/main/java/com/example/android/weatherstorm/app/DetailActivity.java
@@ -1,13 +1,9 @@
 package com.example.android.weatherstorm.app;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -19,9 +15,6 @@ import android.widget.TextView;
  * Created by Aleksandar on 8/10/2016.
  */
 public class DetailActivity extends ActionBarActivity {
-
-            // LOGGER
-    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
 
 
@@ -57,10 +50,7 @@ public class DetailActivity extends ActionBarActivity {
             return true;
         }
 
-        if (id == R.id.action_map) {
-            openPreferredLocationInMap();
-            return true;
-        }
+
         return super.onOptionsItemSelected(item);
     }
 
@@ -88,26 +78,5 @@ public class DetailActivity extends ActionBarActivity {
         }
     }
 
-    private void openPreferredLocationInMap() {
-        SharedPreferences sharedPrefs =
-                PreferenceManager.getDefaultSharedPreferences(this);
-        String location = sharedPrefs.getString(
-                getString(R.string.pref_location_key),
-                getString(R.string.pref_location_default));
 
-        // URI Scheme za locakaciju na mapi, intent iz Common intents na sajtu:
-        // http://developer.android.com/guide/components/intents-common.html#Maps
-        Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
-                .appendQueryParameter("q", location)
-                .build();
-
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.setData(geoLocation);
-
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            startActivity(intent);
-        } else {
-            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
-        }
-    }
 }
